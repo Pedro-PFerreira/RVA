9c52e881154d24d3281dc866f0ecbac554878c79
Pedro-CAB <up202006086@up.pt>
2024-10-28 20:10:07 +0000

    Finished Main Menu and Pause Menu

---
+++ ARProject/Assets/Scripts/PauseMenuLogic.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenuLogic : MonoBehaviour
+{
+    private bool GameIsPaused = true;
+
+    public GameObject PauseMenuUI;
+
+    public PauseButtonLogic PauseButtonUI;
+
+    public void ResumeGame(){
+        Debug.Log("Resuming Game...");
+        PauseButtonUI.ResumeGame();
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        PauseMenuUI.SetActive(false);
+    }
+
+    public void OpenSettings(){
+        Debug.Log("Opening Settings...");
+    }
+
+    public void PauseGame(){
+        Debug.Log("Pausing the Game...");
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void BackToMenu(){
+        Debug.Log("Going Back to Main Menu...");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
