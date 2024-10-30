c82f2487c1464795ae97faf1f9618954ee6a9127
Pedro-CAB <up202006086@up.pt>
2024-10-30 12:24:41 +0000

    Added Endgame and Difficulty Menu UI

---
+++ ARProject/Assets/Scripts/DifficultyMenuLogic.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyMenuLogic : MonoBehaviour
+{
+    public void StartGame(string difficulty){
+        Debug.Log("Starting a " + difficulty + " match...");
+    }
+}
