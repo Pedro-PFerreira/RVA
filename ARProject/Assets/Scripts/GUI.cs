b317370fb3bd96e41c33e34712522e553277583f
Pedro-CAB <up202006086@up.pt>
2024-11-05 10:36:29 +0000

    Refactored Main Menu

---
+++ ARProject/Assets/Scripts/GUI.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUI : MonoBehaviour{
+
+    public enum MenuType{
+        DifficultyMenu,
+        GameOverMenu,
+        MainMenu,
+        PauseButton,
+        PauseMenu,
+        VictoryMenu
+    }
+
+    [SerializeField] MenuType MenuIdentifier;
+
+    void Start(){
+        if (MenuIdentifier == MenuType.MainMenu){
+            PlayerPrefs.SetString("Difficulty","Easy");
+        }
+    }
+    public void StartGame(string difficulty){
+        Debug.Log("Starting a " + difficulty + " match...");
+        PlayerPrefs.SetString("Difficulty",difficulty);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void HowToPlay(){
+        Debug.Log("How to Play Opening...");
+    }
+
+    public void QuitGame(){
+        Debug.Log("Leaving the game...");
+        Application.Quit();
+    }
+
+    public void PlayAgain(){
+        Debug.Log("Restarting the game...");
+        SceneManager.LoadScene("DifficultyMenu");
+    }
+
+    public void GoToMenu(string menu){
+        SceneManager.LoadScene(menu);
+    }
+}
